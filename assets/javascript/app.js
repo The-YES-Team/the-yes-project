184fa9486ff3447e6c7a34db9e066749d5d43e51
Barrett <barrett.allen@icloud.com>
2017-03-21 21:34:29 -0400

    Committing changes, new user form kindof built, but calculator is broken

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+// Defining Global Variables
+
+var userCalorieGoal = 0;
+var userCalorieIntake = 0;
+
+// On Document Load
+
+$(document).ready(function(){
+
+// Loading Animations
+function pulsate() {
+	$("#hero_logo").fadeIn(2000);
+}
+pulsate();
+
+// Creates New or Find Me User buttons
+
+	$("<span>")
+		.addClass("loginItem")
+		.attr("id", "newUserBtn")
+		.append($("<button>").text("New User"))
+		.appendTo("#user-btn-span");
+	$("<span>")
+		.addClass("loginItem")
+		.append($("<h4>").text(" or "))
+		.appendTo("#user-btn-span");
+	$("<span>")
+		.addClass("loginItem")
+		.attr("id", "findUserBtn")
+		.append($("<button>").text("Find Me"))
+		.appendTo("#user-btn-span");
+
+// On button clicks
+
+$("#newUserBtn").on("click", function(event) {
+	$("#user-btn-span").html('form');
+});
+
+
+
+
+
+
+// Closing On Document Load Function
+});
