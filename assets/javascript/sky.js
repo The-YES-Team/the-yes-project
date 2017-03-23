9c100c04d5402070f9cd99e6b441647b1a954196
Skylar Siagel <skylarsiagel@Skylars-MacBook-Pro.local>
2017-03-23 19:09:10 -0400

    prettified buttons

--- assets/javascript/sky.js
+++ assets/javascript/sky.js
@@ -15,7 +15,7 @@
         });
 
         function displayNutrition() {
-        	var food = $("#user-input").val().trim();
+            var food = $("#user-input").val().trim();
             var queryURL = "https://api.nutritionix.com/v1_1/search/" + food + "?results=0:50&fields=item_name,brand_name,nf_calories&appId=9cc304a2&appKey=8d110eedc0d5864ef8a2264a9bbbde73";
 
             $.ajax({
@@ -26,15 +26,29 @@
                 var results = response.hits
                 $("#nutrition-input").empty();
 
-                var newDiv = $("<div>")
-
                 console.log(response);
 
+                var newDiv = $("<div>");
+
                 for (var i = 0; i < results.length; i++) {
-                    newDiv.append("<button>" + results[i].fields.item_name + "<br>" + results[i].fields.brand_name + "<br>" + results[i].fields.nf_calories + "</button>");
+                	var button = $("<button>");
+                	var itemName = results[i].fields.item_name
+                	var brandName = results[i].fields.brand_name
+                	var calorieCount = results[i].fields.nf_calories
+
+                	button.addClass("foodButton");
+                	button.attr("data-name", itemName);
+                	button.attr("data-name", brandName);
+                	button.attr("data-name", calorieCount);
+                	button.append(itemName + "<br>" + brandName + "<br>" + calorieCount);
+
+                	newDiv.append(button);
                     $("#nutrition-input").append(newDiv);
                 }; // end of for loop
-
             }); // end of response function
+            $(".foodButton").on("click", function() {
+            	
+
+            });
         }; // end of displayNutrition
     }); // end of document.ready
