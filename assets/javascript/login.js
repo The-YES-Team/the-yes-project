d94a153b6fc4e6a814c986bbbb76fdc013aa1625
MLangteau <langteauM@gmail.com>
2017-03-24 12:58:52 -0400

    added new user login with Firebase

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -11,23 +11,29 @@
 
 var db = firebase.database();
 
-function writeUserData() {
-  // Creates local "temporary" object for holding user info
 
-  var userCalorieGoal = $("#rc").val();
-  var userName = $("#userName").val();
+// Creates local "temporary" object for holding user info
+
+function saveInfo() {
+  userCalorieGoal = $("#rc").val();
+  userName = $("#userName").val();
+  console.log(userName + userCalorieGoal);
 
   var userInfo = {
       name: userName,
-      calories: userCalories
-  };
+      calories: userCalorieGoal
+  };  
+
 // POSSIBLE FUTURE USE  *********
- /*   age: userAge,
-    gender: userGender,
-    height: userHeight,
-    heightCm: userHeightCm,
-    weight: userWeight,
-    activeSt: userActiveSt  // user activity status */
+//  var userInfo = {
+//    name: userName,
+//    calories: userCalorieGoal
+//    age: userAge,
+//    gender: userGender,
+//    height: userHeight,
+//    heightCm: userHeightCm,
+//    weight: userWeight,
+//    activeSt: userActiveSt  // user activity status 
 
   // Uploads userInfo to the database
 	db.ref().push(userInfo);
@@ -39,37 +45,64 @@ function writeUserData() {
   // Alert
   alert("User Infomation successfully added");
 
-};
-
-writeUserData();
-
-//***********************************************************************************
 //  clears text boxes
-/*  $("#add-user-name").val("");
+ $("#userName").val("");
 
 // Prevent refreshing / moving to a new page
   return false;
 
- }); // end of add-user click
+ }; // end of saveInfo() function (adding a user)
 
-// When user adds entry to database (in other event), this creates Fbase event for 
-//  adding row in html 
 
-db.ref().on("value", function (childSnapshot, prevChildKey) {
+// When searching for a userName (reads Firebase and searches for match)
+//function saveInfo() {            // not really save-just testing
+function getInfo() {         
 
-  //  Store database value of calories into the 
+    var foundIt = false;
 
-  var userName = childSnapshot.val().name;
-  var userCalories = childSnapshot.val().calories;
-  
-  console.log("The following are the four items: ");
-  console.log(userName);
-  console.log(userCalories);
+// change these *********************************************************
+    userName = $("#userName").val();
+    console.log("OUTSIDE UserName: " + userName);
 
-//  var whatever = ???
+    // find all names, whose name = userName
 
-// What to do with new user
+    var db = firebase.database();
 
-});
+    db.ref().orderByChild("name").equalTo(userName).on("child_added", function (childSnapshot) {
+     // works with warning before I set indexOf in rules
 
+    //db.ref().on("child_changed", function (childSnapshot) {  // nope
+    //db.ref().on("value", function (childSnapshot) {  // nope
+
+//    db.ref().on("child_added", function (childSnapshot) { // this also works (consoles all)
+//    above db.ref().on works sync w list (not nec.added) prints them all
+
+    console.log("childSnapshot " + childSnapshot.key);
+
+    //  finds (not userName, but FindMe-userName)  ***************************
+
+    console.log("INSIDE UserName: " + userName);
+
+    selUName = childSnapshot.val().name;
+    selUCalGoal = childSnapshot.val().calories;
+
+    console.log("******selUName: " + selUName + " and selUCalGoal: " + selUCalGoal + "***");
+
+    if (userName == selUName) {
+      userCalorieGoal = selUCalGoal;
+    //  $("#userName").val("");
+    //  will change this when I get the variable from Barrett
+      $("#userName").val(userName+userCalorieGoal);
+      $("#rc").val(userCalorieGoal);
+      console.log("Display name and calories goal: " + selUName, selUCalGoal);
+      foundIt = true;
+//      return;
+    };  // end of if (userName == selUName)
+});  //  end of db.ref().on("child_added", function (childSnapshot)
+
+/*  if (!foundIt) {     // does not work
+  alert("user name not found/Try again, or go to I'm New");
+  console.log("This is the end of function");
+  };
 */
+};  // end of getInfo function
